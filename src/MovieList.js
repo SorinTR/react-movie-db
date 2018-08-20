37debc937103d6058c13f223d33f2ce11c75c560
SorinTR <sorintrofin_2005@yahoo.com>
2018-08-20 20:58:29 +0300

    initial app

---
+++ src/MovieList.js
@@ -0,0 +1,15 @@
+import React, {Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+
+function MovieList() {
+    return (
+        <div>
+        <Button variant="contained" color="primary">
+        Add New Movie
+      </Button>
+        </div>
+    );
+}
+
+export default MovieList;
